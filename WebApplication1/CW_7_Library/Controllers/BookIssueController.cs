f0e6e7cf82e265300ed215f90aae14eb694d6e8d
Mika-shi <ayperiaidarova.com>
2026-09-05 15:07:34 +0600

    Added issued list of books

---
+++ WebApplication1/CW_7_Library/Controllers/BookIssueController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class BookIssueController : Controller
+{
+    private readonly LibraryDbContext _context;
+
+    public BookIssueController(LibraryDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        List<BookIssue> issuedBooks = _context.BookIssues.Include(issue => issue.Book).Include(issue => issue.LibraryUser).Where(issue => issue.ReturnedAt == null).OrderByDescending(issue => issue.IssuedAt).ToList();
+
+        return View(issuedBooks);
+    }
+}
